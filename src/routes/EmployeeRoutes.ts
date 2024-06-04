0e51bf5e696a74a79319385bd37e6eff4b3001bb
dacanizares <dacanizares@outlook.com>
2024-06-04 11:26:52 -0500

    Added delete and toggle employee

--- src/routes/EmployeeRoutes.ts
+++ src/routes/EmployeeRoutes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 
 import { Employee, NewEmployee } from "../domain/models/Employee";
-import { CreateEmployee } from "../commands/EmployeeCommands";
+import { CreateEmployee, DeleteEmployee, ToggleEmployeeStatus } from "../commands/EmployeeCommands";
 import validateModel from "../infrastructure/Validator";
 import { EmployeeViewModel } from "../viewModels/EmployeeViewModels";
 import MapTo from "../infrastructure/Mapper";
@@ -21,6 +21,9 @@ const getEmployeeService = (): EmployeeService => (
 
 router.use(employeeLog);
 
+//
+// POST: employee/
+//
 router.post('/', async (req: Request<{}, {}, CreateEmployee>, res: Response, next: NextFunction) => {
   try {
     const [command, isValid, validationErrors] = validateModel<CreateEmployee>(CreateEmployee, req.body);
@@ -34,7 +37,57 @@ router.post('/', async (req: Request<{}, {}, CreateEmployee>, res: Response, nex
       res.send(
         MapTo<Employee, EmployeeViewModel>(result, EmployeeViewModel)
       );
-    }    
+    }
+  } catch (error) {
+    console.log(`[server]: Path: "/". Body: "${req.body}"`);
+    next(error);
+  }
+});
+
+//
+// POST: employee/togglestatus
+//
+router.post('/togglestatus', async (req: Request<{}, {}, ToggleEmployeeStatus>, res: Response, next: NextFunction) => {
+  try {
+    const [command, isValid, validationErrors] = validateModel<ToggleEmployeeStatus>(ToggleEmployeeStatus, req.body);
+    
+    if (!isValid) {
+      res.status(400).send(validationErrors);
+    } else {
+      const service = getEmployeeService();
+      const result = await service.toggleEmployeeStatus(command);
+      
+      res.send(
+        MapTo<Employee, EmployeeViewModel>(result, EmployeeViewModel)
+      );
+    }
+  } catch (error) {
+    console.log(`[server]: Path: "/". Body: "${req.body}"`);
+    next(error);
+  }
+});
+
+//
+// DELETE: employee/
+//
+router.delete('/', async (req: Request<{}, {}, DeleteEmployee>, res: Response, next: NextFunction) => {
+  try {
+    const [command, isValid, validationErrors] = validateModel<DeleteEmployee>(DeleteEmployee, req.body);
+    
+    if (!isValid) {
+      res.status(400).send(validationErrors);
+    } else {
+      const service = getEmployeeService();
+      const result = await service.deleteEmployee(command);
+      
+      if (result) {
+        res.send(
+          MapTo<Employee, EmployeeViewModel>(result, EmployeeViewModel)
+        );
+      } else {
+        res.status(404).send(`Employee ${command.id} not found.`);
+      }
+    }
   } catch (error) {
     console.log(`[server]: Path: "/". Body: "${req.body}"`);
     next(error);
