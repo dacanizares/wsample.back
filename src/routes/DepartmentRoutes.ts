3bd258890a784b6bcc6e36cd7cc80809fcca9331
dacanizares <dacanizares@outlook.com>
2024-06-04 16:05:35 -0500

    Added route for department by id

--- src/routes/DepartmentRoutes.ts
+++ src/routes/DepartmentRoutes.ts
@@ -4,6 +4,7 @@ import { MapTo, MapAllTo } from "../infrastructure/Mapper";
 import { Department } from "../domain/models/Department";
 import { DepartmentViewModel } from "../viewModels/DepartmentViewModels";
 import DepartmentQueries from "../queries/DepartmentQueries";
+import { tryParseNumber } from "../mappers/BasicMapper";
 
 const router = express.Router();
 
@@ -24,6 +25,32 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
     res.send(
       MapAllTo<Department, DepartmentViewModel>(result, DepartmentViewModel)
     );
+  } catch (error) {
+    console.log(`[server]: Path: "/". Body: "${req.body}"`);
+    next(error);
+  }
+});
+
+
+//
+// GET: department/:id
+//
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = tryParseNumber(req.params['id']);
+    if (!id) {
+      res.status(400).send('id is not a number');
+    } else {
+      const result = await DepartmentQueries.findDepartmentById(id);
+      
+      if (result) {
+        res.send(
+          MapTo<Department, DepartmentViewModel>(result, DepartmentViewModel)
+        );
+      } else {
+        res.status(404).send(`Department ${id} not found.`);
+      }
+    }
   } catch (error) {
     console.log(`[server]: Path: "/". Body: "${req.body}"`);
     next(error);
