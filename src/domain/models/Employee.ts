401c0b74aab235384bbaaf353f93d74d0afb2242
dacanizares <dacanizares@outlook.com>
2024-06-04 09:25:16 -0500

    Added employee domain models

---
+++ src/domain/models/Employee.ts
@@ -0,0 +1,23 @@
+import { ColumnType, Generated, Insertable, Selectable, Updateable } from 'kysely';
+
+export const EMPLOYEE_TABLE = 'employee';
+
+export interface EmployeeTable {
+  id: Generated<number>
+  active: number;
+  firstName: string;
+  lastName: string;
+  hireDate: ColumnType<Date | null, string | null, string | null>;
+  phone: string | null;
+  address: string | null;
+
+  avatarUrl: string | null;
+  
+  // ColumnType<SelectType, InsertType, UpdateType>
+  creationDate: ColumnType<Date, string, never>;
+  modificationDate: ColumnType<Date, string, string>;
+}
+
+export type Employee = Selectable<EmployeeTable>
+export type NewEmployee = Insertable<EmployeeTable>
+export type UpdatedEmployee = Updateable<EmployeeTable>
