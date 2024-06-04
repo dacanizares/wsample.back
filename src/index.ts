5d27a96b437c79bd3e285113b3bf77a7496349bb
dacanizares <dacanizares@outlook.com>
2024-06-04 00:40:54 -0500

    Added basic security measures

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import helmet from "helmet";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+// Standard security measures
+app.use(helmet());
+app.disable('x-powered-by');
 app.get("/", (_req: Request, res: Response) => {
   res.send("SEM Backend");
 });
