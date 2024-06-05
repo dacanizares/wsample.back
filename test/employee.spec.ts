cc94a00513c852caadb805caf1377773372535d2
dacanizares <dacanizares@outlook.com>
2024-06-05 04:42:42 -0500

    Fixed test

--- test/employee.spec.ts
+++ test/employee.spec.ts
@@ -8,17 +8,52 @@ describe("test employees", () => {
     expect(res.status).toEqual(200);
     expect(res.body).toEqual([
       {
-        "active": 1, 
-        "address": null, 
-        "avatarUrl": null, 
+        "id": 1,
+        "active": 1,
+        "firstName": "Dale",
+        "lastName": "Cooper",
+        "hireDate": "1990-01-21",
+        "phone": "911",
+        "address": "Twin Peaks",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg",
         "departmentId": null,
-        "departmentName": null,
-        "firstName": "Dale", 
-        "hireDate": null, 
-        "id": 1, 
-        "lastName": 
-        "Cooper", 
-        "phone": null
+        "departmentName": null
+      },
+      {
+        "id": 2,
+        "active": 1,
+        "firstName": "Windom",
+        "lastName": "Earl",
+        "hireDate": null,
+        "phone": "999",
+        "address": "Black lodge",
+        "avatarUrl": "https://www.twinpeaksblog.com/wp-content/uploads/2021/04/06_TPB_CostumeWindom_MauveZone_2014WindomTable.jpg",
+        "departmentId": null,
+        "departmentName": null
+      },
+      {
+        "id": 3,
+        "active": 1,
+        "firstName": "Shelly",
+        "lastName": "Johnson",
+        "hireDate": "1990-01-21",
+        "phone": "353-54-55",
+        "address": "RR",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/f/f8/Shelly_Johnson_in_Twin_Peaks.png",
+        "departmentId": null,
+        "departmentName": null
+      },
+      {
+        "id": 4,
+        "active": 1,
+        "firstName": "Warren",
+        "lastName": "Frost",
+        "hireDate": "1990-01-21",
+        "phone": "445-88-99",
+        "address": "Hospital",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/2/2e/Warren_Frost_Twin_Peaks_resize.jpg",
+        "departmentId": null,
+        "departmentName": null
       }
     ]);
   });
