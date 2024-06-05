a44f28c94e42da1679ccd5527a88c2cda018a6f6
dacanizares <dacanizares@outlook.com>
2024-06-04 23:30:30 -0500

    Updated seed data

--- src/migrations/001_employees.ts
+++ src/migrations/001_employees.ts
@@ -23,7 +23,39 @@ export async function up(db: Kysely<DatabaseDefinition>): Promise<void> {
 
     // Seed data
     await db.insertInto(EMPLOYEE_TABLE)
-      .values({ "active": 1, "firstName": "Dale", "lastName": "Cooper" } as NewEmployee)
+      .values({ 
+        "active": 1, 
+        "firstName": "Dale", 
+        "lastName": "Cooper",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+      } as NewEmployee)
+      .returningAll()
+      .executeTakeFirstOrThrow();
+    await db.insertInto(EMPLOYEE_TABLE)
+      .values({ 
+        "active": 1, 
+        "firstName": "Windom", 
+        "lastName": "Earl",
+        "avatarUrl": "https://www.twinpeaksblog.com/wp-content/uploads/2021/04/06_TPB_CostumeWindom_MauveZone_2014WindomTable.jpg" 
+      } as NewEmployee)
+      .returningAll()
+      .executeTakeFirstOrThrow();
+    await db.insertInto(EMPLOYEE_TABLE)
+      .values({ 
+        "active": 1, 
+        "firstName": "Shelly", 
+        "lastName": "Johnson",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/f/f8/Shelly_Johnson_in_Twin_Peaks.png" 
+      } as NewEmployee)
+      .returningAll()
+      .executeTakeFirstOrThrow();
+    await db.insertInto(EMPLOYEE_TABLE)
+      .values({ 
+        "active": 1, 
+        "firstName": "Warren", 
+        "lastName": "Frost",
+        "avatarUrl": "https://upload.wikimedia.org/wikipedia/en/2/2e/Warren_Frost_Twin_Peaks_resize.jpg" 
+      } as NewEmployee)
       .returningAll()
       .executeTakeFirstOrThrow();
 }
