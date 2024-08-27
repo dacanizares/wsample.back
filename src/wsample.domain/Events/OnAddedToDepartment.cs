adab6af5f0a6cc99489a919c7a6b40d0055fb234
Juan Felipe Cañizares Corrales <dacanizares@outlook.com>
2024-08-27 00:25:33 -0500

    Implemented mediatr event

---
+++ src/wsample.domain/Events/OnAddedToDepartment.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using wsample.domain.Services;
+
+namespace wsample.domain.Events
+{
+    public class OnAddedToDepartmentEvent : IRequest 
+    {
+        public int EmployeeId { get; set; }
+        public int DepartmentId { get; set; }
+    }
+
+    public class OnAddedToDepartmentHandler : IRequestHandler<OnAddedToDepartmentEvent>
+    {
+        private readonly IHistoryService _historyService;
+
+        public OnAddedToDepartmentHandler(IHistoryService historyService)
+        {
+            _historyService = historyService;
+        }
+        public async Task Handle(OnAddedToDepartmentEvent request, CancellationToken cancellationToken)
+        {
+            await _historyService.CreateHistoryAsync(new Models.History { EmployeeId = request.EmployeeId, DepartmentId = request.DepartmentId });
+        }
+    }
+}
