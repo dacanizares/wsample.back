4c6def544235f51a8779e3d53fcb2679f99f6a37
dacanizares <dacanizares@outlook.com>
2024-08-26 23:55:33 -0500

    Added history controller

---
+++ src/wsample.api/Controllers/HistoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using wsample.api.ViewModels;
+using wsample.domain.Models;
+
+namespace wsample.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+        [HttpGet("{employeeId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<DepartmentViewModel>> GetByIdAsync(string employeeId)
+        {
+            return NoContent();
+        }
+    }
+}
