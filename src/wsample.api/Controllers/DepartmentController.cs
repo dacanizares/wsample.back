a41cac0c370f06616fff7506e4274172fcb4e3e0
dacanizares <dacanizares@outlook.com>
2024-08-26 23:43:40 -0500

    Added sample controller

---
+++ src/wsample.api/Controllers/DepartmentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using wsample.domain.Models;
+
+namespace wsample.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok("OK");
+        }
+    }
+}
