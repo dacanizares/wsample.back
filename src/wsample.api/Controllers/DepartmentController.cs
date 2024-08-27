21c9955f1e6a410bfc0157030cf6146d5b9e0890
dacanizares <dacanizares@outlook.com>
2024-08-27 00:07:04 -0500

    Updated department controller

--- src/wsample.api/Controllers/DepartmentController.cs
+++ src/wsample.api/Controllers/DepartmentController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using wsample.api.Queries;
 using wsample.api.ViewModels;
 using wsample.domain.Models;
+using wsample.domain.Services;
 
 namespace wsample.api.Controllers
 {
@@ -10,19 +12,33 @@ namespace wsample.api.Controllers
     [ApiController]
     public class DepartmentController : ControllerBase
     {
+        private readonly IDepartmentQueries _queries;
+
+        public DepartmentController(IDepartmentQueries queries)
+        {
+            _queries = queries;
+        }
+
+
         [HttpGet]
         [ProducesResponseType(200)]
         public async Task<ActionResult<IEnumerable<DepartmentViewModel>>> GetAll()
         {
-            return NoContent();
+            return await _queries.FindDepartmentsAsync();
         }
 
         [HttpGet("{id}")]
         [ProducesResponseType(200)]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<DepartmentViewModel>> GetByIdAsync(string id)
+        public async Task<ActionResult<DepartmentViewModel>> GetByIdAsync(int id)
         {
-            return NoContent();
+            var result = await _queries.FindDepartmentByIdAsync(id);
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
     }
 }
