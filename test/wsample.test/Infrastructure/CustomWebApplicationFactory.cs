f7e9f09da0b22c2621a941148fd5b145bad3885b
dacanizares <dacanizares@outlook.com>
2024-08-27 01:23:47 -0500

    Added functional test

---
+++ test/wsample.test/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wsample.api.Context;
+
+namespace wsample.test.Infrastructure
+{
+    public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+           builder.UseEnvironment("Development");
+        }
+    }
+}
